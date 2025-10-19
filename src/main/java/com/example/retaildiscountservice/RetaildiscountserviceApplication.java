7eb6bffdd6da31d865d8c9192c7f2c8defd88fd2
Aziz Ibadullah <backop@live.com>
2025-10-19 19:41:13 +0300

    initial commit

---
+++ src/main/java/com/example/retaildiscountservice/RetaildiscountserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.retaildiscountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetaildiscountserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetaildiscountserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/retaildiscountservice/RetaildiscountserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retaildiscountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetaildiscountserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
